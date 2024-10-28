084124daaadad16121c1ccee5ffb3120a07e2ff2
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-10-28 13:16:28 +0100

    insertar registro

--- Java_Mysql/src/main/java/com/oregoon/mensajes/Java_Mysql.java
+++ Java_Mysql/src/main/java/com/oregoon/mensajes/Java_Mysql.java
@@ -1,18 +1,28 @@
 
 package com.oregoon.mensajes;
 import java.sql.*;
+import java.util.Scanner;
 
 public class Java_Mysql {
 
     public static void main(String[] args) throws SQLException {
         
        
-        consultaBaseDatos();
-        
-        
-        
+       
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Por favor, introduzca el mensaje");
+            String mensaje = sc.nextLine();
+            System.out.println("Lo ha escrito por: ");
+            String autor = sc.nextLine();
+
+            insertarRegistro(mensaje, autor);
+            consultaBaseDatos();
+
+            consultaBaseDatos();  // Llama a este método para ver los registros actuales
+ 
        
     }
+    
     
     static void consultaBaseDatos() throws SQLException{
          /*
@@ -41,6 +51,30 @@ public class Java_Mysql {
             System.out.println("Mensaje: "+ mensaje +" es del autor: "+  autor+" y la fecha: "+ fecha);
         }
          rs.close();
+        ps.close();
+        conectar.close();
+    }
+    static void insertarRegistro(String mensaje, String autor) throws SQLException{
+         /*
+        Conexion a base de datos
+        */
+        Connection conectar =DriverManager.getConnection(
+                "jdbc:mysql://localhost/pruebamensajes_db?serverTimezone=UTC", 
+                "root", 
+                "Alvaro0801"
+        );
+        System.out.println("Conexion Exitosa a la base de datos");
+        //Hacer un Select a la base de datos
+        
+        // Se define una cadena SQL para seleccionar todos los registros de la tabla "mensajes"
+        String sql = "INSERT INTO mensajes(mensaje, autor,fecha)VALUES(?,?, CURRENT_TIME());";
+        // Se crea un objeto PreparedStatement utilizando la conexión "conectar" y la consulta SQL definida
+        PreparedStatement ps = conectar.prepareStatement(sql);
+        ps.setString(1, mensaje);
+        ps.setString(2, autor);
+        ps.executeUpdate();
+        
+        
         ps.close();
         conectar.close();
     }
