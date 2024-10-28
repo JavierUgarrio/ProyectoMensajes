0fbd2366d108eb3c46e57e165a6cfc7fbc876dce
JavierUgarrio <j.ugarrio.duro@gmail.com>
2024-10-28 12:56:17 +0100

    consulta desde Java

--- Java_Mysql/src/main/java/com/oregoon/mensajes/Java_Mysql.java
+++ Java_Mysql/src/main/java/com/oregoon/mensajes/Java_Mysql.java
@@ -6,7 +6,16 @@ public class Java_Mysql {
 
     public static void main(String[] args) throws SQLException {
         
-        /*
+       
+        consultaBaseDatos();
+        
+        
+        
+       
+    }
+    
+    static void consultaBaseDatos() throws SQLException{
+         /*
         Conexion a base de datos
         */
         Connection conectar =DriverManager.getConnection(
@@ -15,10 +24,13 @@ public class Java_Mysql {
                 "Alvaro0801"
         );
         System.out.println("Conexion Exitosa a la base de datos");
-        
         //Hacer un Select a la base de datos
+        
+        // Se define una cadena SQL para seleccionar todos los registros de la tabla "mensajes"
         String sql = "SELECT * FROM mensajes";
+        // Se crea un objeto PreparedStatement utilizando la conexión "conectar" y la consulta SQL definida
         PreparedStatement ps = conectar.prepareStatement(sql);
+        // Se ejecuta la consulta y se almacena el resultado en un objeto ResultSet
         ResultSet rs = ps.executeQuery();
         
         while(rs.next()){
@@ -28,8 +40,7 @@ public class Java_Mysql {
             String fecha = rs.getString("fecha");
             System.out.println("Mensaje: "+ mensaje +" es del autor: "+  autor+" y la fecha: "+ fecha);
         }
-        
-        rs.close();
+         rs.close();
         ps.close();
         conectar.close();
     }
